e7ccc94943e0a355ccf7514ef84131300c613c2b
GABRIEL CORREA CORDEIRO <a1763954@debian9-dacom.dacom>
2017-08-30 16:53:12 -0300

    aplicando filtros e procurando bugs

--- src/main/java/q2/Q2Study.java
+++ src/main/java/q2/Q2Study.java
@@ -3,37 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package q1;
+package q2;
 
+import org.eclipse.jgit.lib.Repository;
 import org.repodriller.RepoDriller;
 import org.repodriller.RepositoryMining;
 import org.repodriller.Study;
+import org.repodriller.filter.commit.OnlyInMainBranch;
 import org.repodriller.filter.range.Commits;
 import org.repodriller.persistence.csv.CSVFile;
-import org.repodriller.scm.GitRemoteRepository;
 import org.repodriller.scm.GitRepository;
-
 
 /**
  *
  * @author a1763954
  */
-public class  Q1Stud1 implements Study{
+public class Q2Study implements Study{
+   
     public static void main(String[] args) {
-        new RepoDriller().start(new Q1Stud1() );
+        new RepoDriller().start(new Q2Study());
     }
+ 
     @Override
     public void execute() {
-       new RepositoryMining()
-               .in(GitRemoteRepository.hostedOn("https://github.com/canadaduane/house.git")
-               .buildAsSCMRepository())
-               .through(Commits.all())
-               .process(new CommitVisitor(),new CSVFile("path"))
-               .mine();
-               
-               
-               
-               
+        new RepositoryMining()
+               .in(GitRepository.singleProject("path"))
+                .through(Commits.all())
+                .filters(new OnlyInMainBranch())
+                .process(new BugVisitor(),new CSVFile("path"))
+                .mine();
     }
     
 }
